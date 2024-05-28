577500107cdff3fc8484dbfe35f9db2e7720a4d2
melodyxpot <melodyxpot@gmail.com>
2024-05-28 09:10:18 -0400

    :bug: fixed the bug with config

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,13 @@ const nextConfig = {
       },
       {
         protocol: 'https',
-        hostname: 'powerful-happiness-9b7bd959ea.strapiapp.com',
+        hostname: "powerful-happiness-9b7bd959ea.media.strapiapp.com",
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: "powerful-happiness-9b7bd959ea.strapiapp.com",
         port: '',
         pathname: '/**',
       }

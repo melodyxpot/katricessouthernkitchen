f382e7ca11afd04667afb9898058bcf33a1aec99
melodyxpot <melodyxpot@gmail.com>
2024-06-26 11:36:16 -0400

    :hammer: change facebook url

--- constants.ts
+++ constants.ts
@@ -1,22 +1,22 @@
 export const socials = {
   phone: {
-    text: '7733558503',
-    link: 'tel:+7733558503'
+    text: "7733558503",
+    link: "tel:+7733558503",
   },
   email: {
-    text: 'info@katrices.southern.kitchen@gmail.com',
-    link: 'mailto:info@katrices.southern.kitchen@gmail.com'
+    text: "info@katrices.southern.kitchen@gmail.com",
+    link: "mailto:info@katrices.southern.kitchen@gmail.com",
   },
   facebook: {
-    text: 'Katrices Southern Kitchen',
-    link: ''
+    text: "Katrices Southern Kitchen",
+    link: "https://www.facebook.com/share/GnnYDHw2AFE565ac/?mibextid=LQQJ4d",
   },
   instagram: {
-    text: '@katricessouthernkitchen',
-    link: '@katricessouthernkitchen'
+    text: "@katricessouthernkitchen",
+    link: "@katricessouthernkitchen",
   },
   x: {
-    text: '@katricessouthernkitchen',
-    link: ''
-  }
-}
+    text: "@katricessouthernkitchen",
+    link: "",
+  },
+};
